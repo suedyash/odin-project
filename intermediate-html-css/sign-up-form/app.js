72e09bd08afaa1d4b85818508223377e14df9456
Swyzsh <swyzsh@protonmail.com>
2024-04-21 00:34:54 +0530

    final draft 1

---
+++ intermediate-html-css/sign-up-form/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  /* Hyperlinks */
+  document.getElementById('back-cred-artist').addEventListener('click', () => {
+    window.open("https://unsplash.com/@mosdesign", "_self");
+  });
+  document.getElementById('back-cred-link').addEventListener('click', () => {
+    window.open("https://unsplash.com/photos/a-person-walking-down-a-street-in-front-of-a-store-ild0babk0Q0", "_self");
+  });
+});
