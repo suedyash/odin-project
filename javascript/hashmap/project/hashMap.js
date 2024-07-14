c590b464bd534bcc36f7b948d1be477380fbe828
Swyzsh <swyzsh@protonmail.com>
2024-07-14 21:08:52 +0530

    add resize functions based on loadfactor to hashMap.js

--- javascript/hashmap/project/hashMap.js
+++ javascript/hashmap/project/hashMap.js
@@ -13,11 +13,38 @@ function hash(key, tableSize) {
 class HashMap {
   //table = new Array(389);
   table = new Array(3);
+  numItems = 0;
+
+  resizeTable() {
+    const newTable = new Array(this.table.length * 2);
+
+    this.table.forEach(item => {
+      if (item) {
+        item.forEach(([key, value]) => {
+          const index = hash(key, newTable.length);
+          if (newTable[index]) {
+            newTable[index].push([key, value]);
+          } else {
+            newTable[index] = [[key, value]];
+          }
+        });
+      }
+    });
+
+    this.table = newTable;
+  }
 
   set(key, value) {
+    this.numItems++;
+    
+    const loadFactorFunc = this.numItems / this.table.length;
+    if (loadFactorFunc > 0.75) {
+      this.resizeTable();
+    }
+
     const index = hash(key, this.table.length);
     if (this.table[index]) {
-      this.table[index].push( [key, value] );
+      this.table[index].push([key, value]);
     } else {
       this.table[index] = [[key, value]];
     }
@@ -58,7 +85,7 @@ class HashMap {
   }
 
   length() {
-
+    return this.numItems;
   }
 
   keys() {
@@ -85,6 +112,8 @@ data.set("lastName", "tim");
 data.set("age", 21);
 data.set("dob", "17/04/2003");
 
+console.log("Total Keys: ", data.length());
+console.log("Hash Table Size: ", data.table.length);
 console.log("Here is a list of all the entries:", data.entries());
 console.log(data.get("firstName"));
 console.log(data.get("lastName"));
