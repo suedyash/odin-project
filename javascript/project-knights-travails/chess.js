a8affdaf512eeb4c3a65e528333730bb7d2bcb2e
Swyzsh <swyzsh@protonmail.com>
2025-01-04 18:24:32 +0530

    fin project knight travails

--- javascript/project-knights-travails/chess.js
+++ javascript/project-knights-travails/chess.js
@@ -1,14 +1,59 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var possibleMoves = [
+    [1, 2],
+    [2, 1],
+    [-1, 2],
+    [-2, 1],
+    [-1, -2],
+    [-2, -1],
+    [1, -2],
+    [2, -1],
+];
+function getValidMoves(position) {
+    var x = position[0], y = position[1];
+    return possibleMoves
+        .map(function (_a) {
+        var dx = _a[0], dy = _a[1];
+        return [x + dx, y + dy];
+    }) // Calculate new coordinates
+        .filter(function (_a) {
+        var nx = _a[0], ny = _a[1];
+        return nx >= 0 && nx < 8 && ny >= 0 && ny < 8;
+    }); // Filter out moves off the board
+}
+function bfsKnight(startPoint, endPoint) {
+    var queue = [
+        [startPoint, [startPoint]],
+    ];
+    var _loop_1 = function () {
+        var _a = queue.shift(), current = _a[0], path = _a[1];
+        if (current[0] === endPoint[0] && current[1] === endPoint[1]) {
+            return { value: path };
+        }
+        var moves = getValidMoves(current);
+        moves.forEach(function (move) { return queue.push([move, __spreadArray(__spreadArray([], path, true), [move], false)]); });
+    };
+    while (queue.length > 0) {
+        var state_1 = _loop_1();
+        if (typeof state_1 === "object")
+            return state_1.value;
+    }
+    return [];
+}
 function knightMoves(startPoint, endPoint) {
-    var path = [startPoint];
-    // LOGIC
-    path.push([4, 5]);
-    path.push([2, 4]);
-    path.push(endPoint);
-    var formattedPath = path.map(function (coord) { return "[".concat(coord.join(","), "]"); }).join("\n");
+    var path = bfsKnight(startPoint, endPoint);
+    var formattedPath = path
+        .map(function (coord) { return "[".concat(coord.join(","), "]"); })
+        .join("\n");
     var moves = path.length - 1;
     return console.log("You made it in ".concat(moves, " moves! Here's your path: \n").concat(formattedPath));
 }
 knightMoves([3, 3], [4, 3]);
-// knightMoves([3, 3], [4, 3], [6, 8], [11, 5]);
-// TODO: return shortest possible way to get from one square on chess board to another by outputting all squares the knight will
-// stop on along the way
--- javascript/project-knights-travails/chess.ts
+++ javascript/project-knights-travails/chess.ts
@@ -1,13 +1,45 @@
 type chessCoordType = [number, number];
 
+const possibleMoves: chessCoordType[] = [
+  [1, 2],
+  [2, 1],
+  [-1, 2],
+  [-2, 1],
+  [-1, -2],
+  [-2, -1],
+  [1, -2],
+  [2, -1],
+];
+
+function getValidMoves(position: chessCoordType): chessCoordType[] {
+  const [x, y] = position;
+  return possibleMoves
+    .map(([dx, dy]) => [x + dx, y + dy] as chessCoordType) // Calculate new coordinates
+    .filter(([nx, ny]) => nx >= 0 && nx < 8 && ny >= 0 && ny < 8); // Filter out moves off the board
+}
+
+function bfsKnight(
+  startPoint: chessCoordType,
+  endPoint: chessCoordType,
+): chessCoordType[] {
+  const queue: [chessCoordType, chessCoordType[]][] = [
+    [startPoint, [startPoint]],
+  ];
+
+  while (queue.length > 0) {
+    const [current, path] = queue.shift()!;
+    if (current[0] === endPoint[0] && current[1] === endPoint[1]) {
+      return path;
+    }
+    const moves = getValidMoves(current);
+    moves.forEach((move) => queue.push([move, [...path, move]]));
+  }
+
+  return [];
+}
+
 function knightMoves(startPoint: chessCoordType, endPoint: chessCoordType) {
-  const path: chessCoordType[] = [startPoint];
-
-  // LOGIC
-  path.push([4, 5]);
-  path.push([2, 4]);
-
-  path.push(endPoint);
+  const path = bfsKnight(startPoint, endPoint);
 
   let formattedPath: string = path
     .map((coord) => `[${coord.join(",")}]`)
@@ -21,6 +53,3 @@ function knightMoves(startPoint: chessCoordType, endPoint: chessCoordType) {
 }
 
 knightMoves([3, 3], [4, 3]);
-
-// TODO: return shortest possible way to get from one square on chess board to another by outputting all squares the knight will
-// stop on along the way
